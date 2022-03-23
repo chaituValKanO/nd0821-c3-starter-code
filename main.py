94e288f76557d22b32c3d30d901ae601badaab7a
chaituValKanO <chaitu0406@gmail.com>
2022-03-23 09:29:12 +0530

    Adhering to submission standards

--- main.py
+++ main.py
@@ -38,6 +38,26 @@ class CensusData(BaseModel):
     hours_per_week: float
     native_country: str
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "age": 52,
+                "workclass": "Self-emp-not-inc",
+                "fnlgt": 209642,
+                "education": "HS-grad",
+                "education_num": 9,
+                "marital_status": "Married-civ-spouse",
+                "occupation": "Exec-managerial",
+                "relationship": "Husband",
+                "race": "White",
+                "sex": "Male",
+                "capital_gain": 0,
+                "capital_loss": 0,
+                "hours_per_week": 45,
+                "native_country": "United-States",
+            }
+        }
+
 
 app = FastAPI()
 
